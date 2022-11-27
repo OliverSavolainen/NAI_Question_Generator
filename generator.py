87c1cc9183668ebcd275f7a763799ab2ceb1accf
olisavo <olisavo@ut.ee>
2022-11-27 13:42:57 +0200

    Create starting code for reading and generating

---
+++ generator.py
@@ -0,0 +1,22 @@
+import openai
+
+
+class Generator:
+    def __init__(self, text):
+        self.text = text
+
+    def generate(self, n):
+        openai.api_key = "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"
+
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt="Create a list of " + str(n) + " test questions based on this text:\n\nText: " + self.text + "\n\n",
+            temperature=0.5,
+            max_tokens=300,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        questions = response["choices"][0]["text"]
+        return questions
---
+++ main.py
@@ -0,0 +1,35 @@
+from generator import Generator
+from pdfReader import PDFReader
+
+
+def main():
+    f = open("all_questions.txt", "a")
+
+    while True:
+        filename = input("Enter the PDF filename you want questions from: ")
+        try:
+            if filename == "end":
+                break
+            else:
+                pdfReader = PDFReader(filename)
+
+                text = pdfReader.read()
+
+                generator = Generator(text)
+
+                n = input("Enter the number of questions you want: ")
+
+                response = generator.generate(n)
+
+                print(response)
+
+                f.write("\n" + filename + "\n")
+                f.write(response + "\n")
+        except:
+            print("File was not found, enter again")
+    print("Exiting the program")
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
