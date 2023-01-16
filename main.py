7006b1a06ddb628378c6a61e9eef88ce5c5469a9
monitor640 <fox.kuuse@gmail.com>
2023-01-16 19:25:58 +0200

    added translations

--- main.py
+++ main.py
@@ -100,17 +100,17 @@ def promptSelected(prompt, raam, entry):
 def main():
     # f = open("all_questions.txt", "a")
     raam = tkinter.Tk()
-    raam.title("Testi genereerija")
+    raam.title("Test generator")
     raam.geometry("800x500")
     raam.resizable(False, False)
 
     # add a box to enter openai.api_key
-    api_key_label = tkinter.Label(raam, text="Sisesta oma API võti:")
+    api_key_label = tkinter.Label(raam, text="Enter your API key:")
     api_key_entry = tkinter.Entry(raam, width=50)
     api_key_label.place(x=10, y=10)
     api_key_entry.place(x=10, y=30)
     # add a save button
-    savebutton = tkinter.Button(raam, text="Salvesta", command=lambda: save_api_key(api_key_entry.get()))
+    savebutton = tkinter.Button(raam, text="Save", command=lambda: save_api_key(api_key_entry.get()))
     # add save button to the right of the entry box
     savebutton.place(x=350, y=30)
 
@@ -120,15 +120,15 @@ def main():
     else:
         api_key_entry.insert(0, get_api_key())
     # add a box to enter the number of questions
-    question_number_label = tkinter.Label(raam, text="Sisesta küsimuste arv:")
+    question_number_label = tkinter.Label(raam, text="Enter the amount of requested qestions:")
     question_number_entry = tkinter.Entry(raam, width=50)
     question_number_label.place(x=10, y=90)
     question_number_entry.place(x=10, y=110)
 
     # add a box to upload the input file
-    input_file_label = tkinter.Label(raam, text="Vali sisendfail:")
+    input_file_label = tkinter.Label(raam, text="Choose an input file:")
     input_file_entry = tkinter.Entry(raam, width=50)
-    input_file_button = tkinter.Button(raam, text="Vali fail",
+    input_file_button = tkinter.Button(raam, text="Choose file",
                                        command=lambda: input_file_entry.insert(0, fd.askopenfilename()))
     input_file_label.place(x=10, y=150)
     input_file_entry.place(x=10, y=170)
@@ -137,7 +137,7 @@ def main():
     options = ["Create exactly {n} different questions based on this text",
                "Create {n} multiple choice question with the answer and the question", "Custom prompt"]
     # create a drop down menu of options and call the promptSelected function when the user selects an option
-    prompt_label = tkinter.Label(raam, text="Vali küsimuse tüüp:")
+    prompt_label = tkinter.Label(raam, text="Choose the prompt:")
     prompt_variable = tkinter.StringVar(raam)
     prompt_variable.set(options[0])
     prompt_menu = tkinter.OptionMenu(raam, prompt_variable, *options)
@@ -148,12 +148,12 @@ def main():
     entry_customprompt = tkinter.Entry(raam, width=50)
     label_customprompt.place(x=10, y=270)
     entry_customprompt.place(x=10, y=290)
-    save_prompt_button = tkinter.Button(raam, text="Salvesta",
+    save_prompt_button = tkinter.Button(raam, text="Save",
                                         command=lambda: promptSelected(prompt_variable.get(), raam, entry_customprompt))
     save_prompt_button.place(x=350, y=230)
 
     # add button to generate questions
-    generate_button = tkinter.Button(raam, text="Genereeri küsimused",
+    generate_button = tkinter.Button(raam, text="Generate questions",
                                      command=lambda: generateQuestions(input_file_entry.get(), api_key_entry.get(),
                                                                        selectedPrompt,
                                                                        int(question_number_entry.get())))
