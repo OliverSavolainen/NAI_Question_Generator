2aa1c1daab70e4465c9455be1991cb69e3f77244
Simon Fox Kuuse <fox.kuuse@gmail.com>
2023-01-16 21:12:10 +0200

    Delete testRegex.py

--- testRegex.py
+++
@@ -1,9 +0,0 @@
-import re
-def main():
-    f = open("test.txt", "r")
-    questions = f.read()
-    new_string = re.sub(r"^Q\s", "", questions, flags=re.MULTILINE)
-    print(new_string)
-
-if __name__ == "__main__":
-    main()
